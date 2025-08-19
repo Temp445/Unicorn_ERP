9849bacdb6e5a99c7cc7707408ffe7c70af896e4
Temp445 <acetemp345@gmail.com>
2025-08-19 10:15:55 +0530

    new commit

--- src/app/api/products/product/[productpath]/route.ts
+++ src/app/api/products/product/[productpath]/route.ts
@@ -1,35 +1,35 @@
-// import { NextResponse } from "next/server";
-// import dbConnect from "@/lib/mongoose";
-// import Product from "@/models/Product";
+import { NextResponse } from "next/server";
+import dbConnect from "@/lib/mongoose";
+import Product from "@/models/Product";
 
-// export async function GET( _req: Request,{ params }: { params: { productpath: string } }) {
-//   try {
-//     await dbConnect();
+export async function GET( _req: Request,{ params }: { params: { productpath: string } }) {
+  try {
+    await dbConnect();
 
-//     const { productpath } = params;
+    const { productpath } = params;
 
-//     if (!productpath || typeof productpath !== "string") {
-//       return NextResponse.json(
-//         { success: false, message: "Invalid product path" },
-//         { status: 400 }
-//       );
-//     }
+    if (!productpath || typeof productpath !== "string") {
+      return NextResponse.json(
+        { success: false, message: "Invalid product path" },
+        { status: 400 }
+      );
+    }
 
-//     const product = await Product.findOne({ productPath: productpath });
+    const product = await Product.findOne({ productPath: productpath });
 
-//     if (!product) {
-//       return NextResponse.json(
-//         { success: false, message: "Product not found" },
-//         { status: 404 }
-//       );
-//     }
+    if (!product) {
+      return NextResponse.json(
+        { success: false, message: "Product not found" },
+        { status: 404 }
+      );
+    }
 
-//     return NextResponse.json({ success: true, data: product });
-//   } catch (err: any) {
-//     console.error("Error in getProductByPath:", err);
-//     return NextResponse.json(
-//       { success: false, message: err.message || "Internal Server Error" },
-//       { status: 500 }
-//     );
-//   }
-// }
+    return NextResponse.json({ success: true, data: product });
+  } catch (err: any) {
+    console.error("Error in getProductByPath:", err);
+    return NextResponse.json(
+      { success: false, message: err.message || "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
